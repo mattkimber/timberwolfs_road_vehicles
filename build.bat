f8646677d7edaeb9192b21b2da249607c0663851
Matt Kimber <github@mattkimber.org.uk>
2018-08-11 16:21:45 +0100

    Added cargo trams

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-cat header/*.pnml templates/*.pnml trailers/*.pnml horse_drawn/*.pnml vehicles/buses/*.pnml vehicles/small/*.pnml vehicles/goods/*.pnml vehicles/trams/*.pnml > timberwolf_uk.prenml
+cat header/*.pnml templates/*.pnml trailers/*.pnml horse_drawn/*.pnml vehicles/buses/*.pnml vehicles/small/*.pnml vehicles/goods/*.pnml vehicles/cargo_trams/*.pnml vehicles/trams/*.pnml > timberwolf_uk.prenml
 "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\cl" /E timberwolf_uk.prenml > timberwolf_uk.nml
 ..\nml\nmlc timberwolf_uk.nml
--- cpp_build.bat
+++ cpp_build.bat
@@ -1,3 +1,3 @@
-cat header/*.pnml templates/*.pnml trailers/*.pnml horse_drawn/*.pnml vehicles/buses/*.pnml vehicles/small/*.pnml vehicles/goods/*.pnml vehicles/trams/*.pnml > timberwolf_uk.prenml
+cat header/*.pnml templates/*.pnml trailers/*.pnml horse_drawn/*.pnml vehicles/buses/*.pnml vehicles/small/*.pnml vehicles/goods/*.pnml vehicles/cargo_trams/*.pnml vehicles/trams/*.pnml > timberwolf_uk.prenml
 cpp timberwolf_uk.prenml > timberwolf_uk.nml
 ..\nml\nmlc timberwolf_uk.nml
