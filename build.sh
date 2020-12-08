6fea8a84fc9eebcb73aaa8854442239fdcba19ae
Matt Kimber <github@mattkimber.org.uk>
2020-12-08 21:06:34 +0000

    build and render update

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 mkdir -p intermediate
 
-./cargopositor.exe -o intermediate/output_fp -v voxels/composite -t compositor/first_pass/*
-./cargopositor.exe -o intermediate/voxels/ -v voxels/composite -t compositor/second_pass/*
+echo "Compositing (1st round)"
+../cargopositor/cargopositor.exe -o intermediate/output_fp -v voxels/composite -t compositor/first_pass/*
 
+echo "Compositing (2nd round)"
+../cargopositor/cargopositor.exe -o intermediate/voxels/ -v voxels/composite -t compositor/second_pass/*
+
+echo "Copying static objects"
 cp voxels/static/* intermediate/voxels
 
-for i in `ls intermediate/voxels`; do 
-    fn=`echo 4x/${i}_32bpp.png | sed -e s/.vox//`
+echo "Rendering outputs"
+echo ""
+ls intermediate/voxels/*.vox | xargs ../gorender/renderobject.exe -r -s 4,2,1 -u -p
 
-    if [ -e $fn ]; then 
-        echo "$i [cached]"
-    else
-        echo "$i [new]"
-	    ../gorender/renderobject.exe -i intermediate/voxels/$i -o $i -s 4,2,1 -u
-    fi
-done
+echo "Producing SD set"
+../roadie/roadie.exe set.json && ../nml/nmlc.exe -c timberwolf_uk.nml
 
-./roadie.exe set.json && ../nml/nmlc.exe -c timberwolf_uk.nml
-./roadie.exe set_hd.json && ../nml/nmlc.exe -c timberwolf_uk_hd.nml
+echo "Producing HD set"
+../roadie/roadie.exe set_hd.json && ../nml/nmlc.exe -c timberwolf_uk_hd.nml
