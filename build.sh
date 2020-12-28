b01d93dcdfb7b476f402e31f67c1383c7bdc274d
Matt Kimber <github@mattkimber.org.uk>
2020-12-28 21:26:41 +0000

    start creating v3

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ echo "Compositing (2nd round)"
 ../cargopositor/cargopositor.exe -o intermediate/voxels/ -v voxels/composite -t compositor/second_pass/*
 
 echo "Copying static objects"
-cp voxels/static/* intermediate/voxels
+cp -u voxels/static/* intermediate/voxels
 
 echo "Rendering outputs"
 echo ""
-ls intermediate/voxels/*.vox | xargs ../gorender/renderobject.exe -r -s 4,2,1 -u -p
+ls intermediate/voxels/*.vox | xargs ../gorender/renderobject.exe -8 -r -s 2,1 -u -p
 
-echo "Producing SD set"
-../roadie/roadie.exe set.json && ../nml/nmlc.exe -c timberwolf_uk.nml
+echo "Producing NML"
+../roadie/roadie.exe set.json
 
-echo "Producing HD set"
-../roadie/roadie.exe set_hd.json && ../nml/nmlc.exe -c timberwolf_uk_hd.nml
+echo "Compiling GRF"
+../nml/nmlc.exe -c timberwolf_uk.nml
